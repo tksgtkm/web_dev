24dee8419a191b4921fe6d6a6904c316a0c384b8
tksgtkm <h9cd342fd7jgx36@gmail.com>
2023-06-12 01:40:41 +0900

    コンポーネント作成はじめ

--- react-sample/src/index.tsx
+++ react-sample/src/index.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Hello from './components/Hello'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <h1>見出し</h1>
+  // <App />
+  <Hello />
 );
 
 // If you want to start measuring performance in your app, pass a function
